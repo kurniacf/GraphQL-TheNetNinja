770e0b5d8020b934a5f16cc5aadd4822ded00497
kurniacf <kcf170202@gmail.com>
2022-01-04 20:27:35 +0700

    Cast Type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql');
 const lodash = require('lodash');
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt} = graphql;
 
 // Dummy Test Data
 let movies = [
@@ -19,6 +19,39 @@ let movies = [
         name: 'Loki',
         genre: 'Action',
         id: '3'
+    },
+    {
+        name: 'Thor: Ragnarock',
+        genre: 'Action',
+        id: '4'
+    },
+    {
+        name: 'Free Guy',
+        genre: 'Sci-Fi',
+        id: '5'
+    }
+];
+
+let casts = [
+    {
+        name: 'Tom Hiddleston',
+        age: 40,
+        id: '1'
+    },
+    {
+        name: 'Ryan Reynolds',
+        age: 45,
+        id: '2'
+    },
+    {
+        name: 'Zooey Deschanel',
+        age: 41,
+        id: '3'
+    },
+    {
+        name: 'Tom Holland',
+        age: 25,
+        id: '4'
     }
 ];
 
@@ -26,13 +59,28 @@ const MovieType = new GraphQLObjectType({
     name: 'Movie',
     fields: ()=>({
         id: {
-            type: GraphQLString
+            type: GraphQLID
         },
         name: {
             type: GraphQLString
         },
         genre: {
             type: GraphQLString
+        }
+    })
+});
+
+const CastType = new GraphQLObjectType({
+    name: 'Cast',
+    fields: ()=>({
+        id: {
+            type: GraphQLID
+        },
+        name: {
+            type: GraphQLString
+        },
+        age: {
+            type: GraphQLInt
         }
     })
 });
@@ -44,12 +92,26 @@ const RootQuery = new GraphQLObjectType({
             type: MovieType,
             args: {
                 id:{
-                    type:GraphQLString
+                    type:GraphQLID
                 }
             },
             resolve(parent, args){
                 // Get Data from DB
+                // console.log(typeof(args.id)) // --> String
                 return lodash.find(movies, {
+                    id: args.id
+                });
+            }
+        },
+        cast: {
+            type: CastType,
+            args: {
+                id: {
+                    type: GraphQLID
+                }
+            },
+            resolve(parent, args){
+                return lodash.find(casts, {
                     id: args.id
                 });
             }
